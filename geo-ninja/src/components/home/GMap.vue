503ede8391cb06540065ff4f6f0f794caebc8c9e
Md Kawsar Hussen <kawsarhussen@gmail.com>
2020-04-01 17:40:22 -0400

    Gmap implemented

--- geo-ninja/src/components/home/GMap.vue
+++ geo-ninja/src/components/home/GMap.vue
@@ -1,15 +1,44 @@
 <template>
   <div class="map">
-    <h2>Map</h2>
+    <div class="google-map" id="map"></div>
   </div>
 </template>
 
 <script>
 export default {
   name: "GMap",
-  data() {}
+  data() {
+    return {
+      lat: 40.73,
+      lng: -73.93
+    };
+  },
+  methods: {
+    renderMap() {
+      const map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: this.lat, lng: this.lng },
+        zoom: 6,
+        maxZoom: 15,
+        minZoom: 3,
+        streetViewControl: false
+      });
+    }
+  },
+  mounted() {
+    this.renderMap();
+  }
 };
 </script>
 
 <style scoped>
+.google-map {
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+  background: #fff;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+}
 </style>
