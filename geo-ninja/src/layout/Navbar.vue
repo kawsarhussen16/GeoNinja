f8fb8f6ca5fa701cd1db1f13903cc6a95939c208
Md Kawsar Hussen <kawsarhussen@gmail.com>
2020-03-31 22:31:33 -0400

    Navbar created and necessary library downloaded

---
+++ geo-ninja/src/layout/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="navbar">
+    <nav class="deep-purple darken-1">
+      <div class="container">
+        <a href="#">GeoNinjas</a>
+        <ul class="right">
+          <li>
+            <a href="#">SignUp</a>
+          </li>
+          <li>
+            <a href="#">Login</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {};
+  }
+};
+</script>
